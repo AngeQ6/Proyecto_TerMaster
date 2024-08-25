a7283e95ed4ce07a8b3dd0d6b41e59a0f463ad2d
Jesus1128 <cvharrysanjuan@gmail.com>
2024-08-24 22:09:17 -0500

    prueba

--- TerMasterr/Controllers/ConductorController.cs
+++ TerMasterr/Controllers/ConductorController.cs
@@ -17,10 +17,6 @@ namespace TerMasterr.Controllers
         {
             return View();
         }
-
-        public ActionResult Registrar_entrada()
-        {
-            return View();
-        } 
+ 
     }
 }
